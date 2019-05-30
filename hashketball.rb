176a59d36d089679cc692a11a68db317f0aab6a5
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-30 01:47:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -93,6 +93,8 @@ require 'pp'
 
 pp PLAYERS.keys
 
+pp player_stats("Jeff Adrien")
+
 # * Build a method, `big_shoe_rebounds`, that will return the number of rebounds associated with the player that has the largest shoe size. Break this one down into steps:
 #   * First, find the player with the largest shoe size
 #   * Then, return that player's number of rebounds
